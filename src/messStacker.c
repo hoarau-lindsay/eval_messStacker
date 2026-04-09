fa19644dd0b24f4da764804fa8e85e7c459ad021
lindsay <hoarau.lly@gmail.com>
2026-04-09 11:29:08 +0000

    fin de l'exercice 10

--- src/messStacker.c
+++ src/messStacker.c
@@ -2,6 +2,16 @@
 
 #include "messStacker.h"
 
+#define SIZE_STACK 30
+
+//initialisation variables globales 
+
+Message stackMess[SIZE_STACK];  //file de message (tableau circulaire)
+int lastPos = -1;               //position du dernier message (le + recent)
+int curPos = 0;                 //message courant 
+int messCount = 0;              //nombre de message présent 
+
+
 char* getMessStackerVersion(){
     return MESS_STACKER_VERSION;
 }
